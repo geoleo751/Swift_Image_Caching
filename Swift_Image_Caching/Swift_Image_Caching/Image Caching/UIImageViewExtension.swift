76519ec644208edcc0057ba7bb5233952747c488
George Leonidas <george.leonidas@atcom.gr>
2019-07-10 16:59:18 +0300

    Added functionality to manpulate URLSession from the UIImageView, Swift 5.0

--- Swift_Image_Caching/Swift_Image_Caching/Image Caching/UIImageViewExtension.swift
+++ Swift_Image_Caching/Swift_Image_Caching/Image Caching/UIImageViewExtension.swift
@@ -6,15 +6,26 @@
 import Foundation
 import UIKit
 
+private var kSessionPropertyKey = "ImageViewCacheSession"
+
 public extension UIImageView {
+    
+    var session : URLSession? {
+        get {
+            return objc_getAssociatedObject(self, &kSessionPropertyKey) as? URLSession
+        }
+        set {
+            objc_setAssociatedObject(self, &kSessionPropertyKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
     
     /// Set Image on an ImageView from cahce if exists, otherwise make the call with the given imageURL
     ///
     /// - Parameters:
     ///   - imageURL: The URL of the image that needs to be retrieved
-    ///   - placehoder: A placeholder for the image
+    ///   - placeholder: A placeholder for the image
     ///   - toBeCahced: Boolean value if image needs to be cached
-    public func setImage(withImageURL imageURL: String?, placeholder: UIImage? = nil, toBeCached: Bool = true, completionBlock: ((Bool) -> Void)? = nil) {
+    func setImage(withImageURL imageURL: String?, placeholder: UIImage? = nil, toBeCached: Bool = true, completionBlock: ((Bool) -> Void)? = nil) {
         /// Set placeholder as background image
         self.image = placeholder
         
@@ -24,39 +35,37 @@ public extension UIImageView {
             /// Chaeck if image exists in Cache
             if let image = CacheHelper.get(withIdentifier: imageURL) {
                 self.image = image
-                if let completionBlock = completionBlock {
-                    completionBlock(true)
-                }
+                completionBlock?(true)
             } else {
                 
-                guard let url = URL(string: imageURL.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? "") else {
+                var urlObject: URL?
+                if let safeURL = URL(string : imageURL) {
+                    urlObject = safeURL
+                } else if let percentEncodedUrl = URL(string : imageURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "") {
+                    urlObject = percentEncodedUrl
+                }
+                
+                guard let url = urlObject else {
                     print("Image Cacher: String URL provided for image: \(self.image?.description ?? "") is not valid URL")
-                    if let completionBlock = completionBlock {
-                        completionBlock(false)
-                    }
+                    completionBlock?(false)
                     return
                 }
                 
-                let session = URLSession(configuration: .ephemeral)
-                
+                self.session = URLSession(configuration: .ephemeral)
                 //creating a dataTask
-                let getImageFromUrl = session.dataTask(with: url) { (data, response, error) in
+                let getImageFromUrl = session?.dataTask(with: url) { (data, response, error) in
                     
                     if let e = error {
-                        if let completionBlock = completionBlock {
-                            completionBlock(false)
-                        }
+                        completionBlock?(false)
                         // handling error
-                        print(e)
+                        print(e.localizedDescription)
                     } else {
                         //in case of no error, checking wheather the response is nil or not
                         if response as? HTTPURLResponse != nil {
-                            
                             //checking if the response contains an image
                             if let imageData = data {
                                 // Create the UIImage
                                 if let resultImage = UIImage(data: imageData) {
-                                    
                                     // Displaying the image in Main Thread
                                     DispatchQueue.main.async {
                                         /// Store image to Cache if we need it
@@ -64,28 +73,29 @@ public extension UIImageView {
                                             CacheHelper.set(resultImage, withIdentifier: imageURL)
                                         }
                                         self.image = resultImage
-                                        if let completionBlock = completionBlock {
-                                            completionBlock(true)
-                                        }
+                                        completionBlock?(true)
                                     }
                                 }
                             } else {
-                                if let completionBlock = completionBlock {
-                                    completionBlock(false)
-                                }
+                                completionBlock?(false)
                                 print("Image Cacher: Image file is currupted")
                             }
                         } else {
-                            if let completionBlock = completionBlock {
-                                completionBlock(false)
-                            }
+                            completionBlock?(false)
                             print("Image Cacher: No response from server")
                         }
                     }
                 }
-                //starting the download task
-                getImageFromUrl.resume()
+                getImageFromUrl?.resume()
             }
         }
+    }
+    
+    func cancelPendingTasks() {
+        self.session?.invalidateAndCancel()
+    }
+    
+    func finishTasksAndInvalidate() {
+        self.session?.finishTasksAndInvalidate()
     }
 }
